ab41b05a0a70a27750fecae501bf5f07fa070a25
Shreyas <shreyasjaiswal1515@gmail.com>
2025-04-05 20:09:16 +0530

    BACKEND BASICS NOTES

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const path = require("path");
 const fs = require("fs");
 const { log } = require("console");
+const { fileLoader } = require("ejs");
 
 app.set("view engine", "ejs");
 app.use(express.json());
@@ -15,6 +16,36 @@ app.get("/", function (req, res) {
   });
 });
 
+app.get("/file/:filename", function (req, res) {
+  fs.readFile(
+    `./files/${req.params.filename}`,
+    "utf-8",
+    function (err, filedata) {
+      res.render("show", {
+        filename: req.params.filename,
+        filedata: filedata,
+      });
+    }
+  );
+});
+
+
+
+app.get("/edit/:filename", function (req, res) {
+      
+        res.render('edit',{filename: req.params.filename});
+  });
+  
+
+  app.post("/edit", function (req, res) {
+      fs.rename(`./files/${req.body.previous}`,`./files/${req.body.new}`,function(err){
+        res.redirect("/");
+      });
+   
+});
+
+
+
 app.post("/create", function (req, res) {
   fs.writeFile(
     `./files/${req.body.title.split(" ").join("")}.txt`,
@@ -24,5 +55,9 @@ app.post("/create", function (req, res) {
     }
   );
 });
+
+
+
+
 
 app.listen(4000);
